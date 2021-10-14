4deb3206f5ed180ac8b4a3e87f2b34ea9a3e744b
Iago D'Ippolito <you@example.com>
2021-10-14 20:59:57 -0300

    Configurando banco de dados e validação de login

--- Livraria/C#/Livraria/Program.cs
+++ Livraria/C#/Livraria/Program.cs
@@ -16,7 +16,7 @@ namespace Livraria
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMenu());
+            Application.Run(new Login());
         }
     }
 }
--- Livraria/C#/Login.cs
+++ Livraria/C#/Login.cs
@@ -17,11 +17,12 @@ namespace Livraria
         public Login()
         {
             InitializeComponent();
+            
         }
 
         // Estabelecendo conexão com o banco de dados SQLServer
 
-        SqlConnection cn = new SqlConnection(@"Data Source=LAPTOP-ODU8ATV0\SQLEXPRESS;integrated security=SSPI;Catalog=db_livraria");
+        SqlConnection cn = new SqlConnection(@"Data Source=LAPTOP-ODU8ATV0\SQLEXPRESS;integrated security=SSPI;initial Catalog=db_livraria");
 
         SqlCommand cm = new SqlCommand(); // envia instruções para o db
         SqlDataReader dt; // só funciona com "select"
@@ -70,6 +71,50 @@ namespace Livraria
 
         }
 
-        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(txtLogin.Text == "" || txtSenha.Text == "")
+            {
+                MessageBox.Show("Obrigatório informar os campos de login e senha!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                try
+                {
+                    cn.Open();
+                    cm.CommandText = "select * from tbl_atendente where ds_Login = ('" + txtLogin.Text + "') and ds_Senha = ('" + txtSenha.Text + "')";
+                    cm.Connection = cn;
+                    dt = cm.ExecuteReader();
+
+                    if(dt.HasRows)
+                    {
+                        frmMenu menu = new frmMenu();
+                        menu.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuário ou senha incorretos!", "Credenciais inválidas!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        txtLogin.Clear();
+                        txtSenha.Clear();
+                        txtLogin.Focus();
+                    }
+                }
+                catch (Exception erro)
+                {
+                    MessageBox.Show(erro.Message);
+                    cn.Close();
+                }
+                finally
+                {
+                    cn.Close();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
